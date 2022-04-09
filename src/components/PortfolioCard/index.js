e099c291a0f13b4bc67b1d6dc29dfcdb19b53f90
jmurphy0 <murphbot55@gmail.com>
2022-04-09 14:06:43 -0400

    styling portfolio links

--- src/components/PortfolioCard/index.js
+++ src/components/PortfolioCard/index.js
@@ -5,11 +5,11 @@ const index = ({ img, name, liveLink, description, title, githubLink }) => {
   return (
     <div className="card__container">
       <div className="card" class="card bg-dark">
-        <a href={liveLink} target="_blank">
-          <img src={img} class="img-thumbnail" alt={name} />
+        <a href={githubLink} title="open github repository" target="_blank">
+          <h3>{title}</h3>
         </a>
-        <a href={githubLink} target="_blank">
-          <p>{title}</p>
+        <a href={liveLink} title="open live link" target="_blank">
+          <img src={img} class="img-thumbnail" alt={name} />
         </a>
         <p className="description">{description}</p>
       </div>
