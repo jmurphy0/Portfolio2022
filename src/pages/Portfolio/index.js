6174015970b1010ba0f1b94f0489dc51968285aa
jmurphy0 <murphbot55@gmail.com>
2022-04-14 14:07:00 -0400

    cleaning up

--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import "./style.css";
-// importing required components
-import Porjectdata from "../../utils/Projectdata";
 import PortfolioCard from "../../components/PortfolioCard";
 import Projectdata from "../../utils/Projectdata.js";
 
