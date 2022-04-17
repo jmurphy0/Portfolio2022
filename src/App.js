e172fc1d7d6780984bfac1432b26d660eb22fb6c
jmurphy0 <murphbot55@gmail.com>
2022-04-17 17:00:13 -0400

    link issues

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Navbar />
         <div class="content">
